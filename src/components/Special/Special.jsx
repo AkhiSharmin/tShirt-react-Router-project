85267f14ed100fd70e1814e0512b05d06abe6b35
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-09 09:43:46 +0600

    orderReview page done

---
+++ src/components/Special/Special.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Special = ({ring}) => {
+    return (
+        <div>
+            <h2>Special</h2>
+            <p>{ring} Ring</p>
+        </div>
+    );
+};
+
+export default Special;
