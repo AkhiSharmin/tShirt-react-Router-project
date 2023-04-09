bfd6d7460880a548b1b14473ed3bb1ac41aa9ee7
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-09 10:32:35 +0600

    props driling done

--- src/components/Sister/Sister.jsx
+++ src/components/Sister/Sister.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MoneyContext } from '../Grandpa/Grandpa';
 
 const Sister = () => {
+    const [money, setMoney] = useContext(MoneyContext)
+
+
     return (
         <div>
             <h2>Sister</h2>
+            <p><small>Grandpa Money: {money}</small></p>
+            <button onClick={() => setMoney(money + 500)}><small>Send 500Tk</small></button>
         </div>
     );
 };
--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Special = ({ring}) => {
+    
+    const angti = useContext(RingContext);
+
     return (
         <div>
             <h2>Special</h2>
             <p>{ring} Ring</p>
+            <small>{angti}</small>
         </div>
     );
 };
